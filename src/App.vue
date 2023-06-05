3dc19af55dd1ab6e0826b43cf0d424c077af2d65
Dmitry Demyanov <dimaprostokosmos@gmail.comnpm run servegit config --global user.email dimaprostokosmos@gmail.com>
2023-06-05 21:58:15 +0300

    start header

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,39 @@
 <template>
   <section>
+    <div class="wrapper-header" >
+      <HeaderComponent />
+    </div>
+
     <!-- <router-link class="p-2" to="/">Главная</router-link>  -->
 
 
     <router-view class="main-view"></router-view>
+    <div class="test-footer"></div>
   </section>
 </template>
 
 <script>
+
+
+import HeaderComponent from '@/components/HeaderComponent';
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    HeaderComponent
+  },
+  
 }
 </script>
 
 <style>
 @import "@/assets/style/index.scss";
 
-.container-normal{
+.container-normal {
   width: 1232px;
   margin: 0 auto;
+}
+
+.test-footer {
+  height: 500px;
 }
 </style>
