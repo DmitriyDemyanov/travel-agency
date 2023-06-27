782691fdeb39e5835e06d33c24fabad514acbc42
Hennadii Khlepitko <khlepitko.gennady@gmail.com>
2023-06-27 12:22:48 +0300

    restore old state.js

--- src/store/modules/globalContent/state.js
+++ src/store/modules/globalContent/state.js
@@ -1,4 +1,267 @@
 export default {
+  //_____________________________uses-footer
+  socialMedia: [
+    {
+      icon: 'facebook',
+      link: 'https://facebook.com'
+    },
+    {
+      icon: 'twitter',
+      link: 'https://twitter.com'
+    },
+    {
+      icon: 'youtube',
+      link: 'https://youtube.com'
+    },
+    {
+      icon: 'instagram',
+      link: 'https://instagram.com'
+    },
+  ],
+  //_________________________________________________FOOTER_LINK
+  footerLinks: [
+    {
+      title: 'Our Destinations',
+      links: [
+        {
+          textLink: 'Canada',
+          link: '#'
+        },
+        {
+          textLink: 'Alaska',
+          link: '#'
+        },
+        {
+          textLink: 'France',
+          link: '#'
+        },
+        {
+          textLink: 'Iceland',
+          link: '#'
+        }
+      ],
+    }
+    ,
+    {
+      title: 'Our Activities',
+      links: [
+        {
+          textLink: 'Northern Lights',
+          link: '#'
+        },
+        {
+          textLink: 'Cruising & sailing',
+          link: '#'
+        },
+        {
+          textLink: 'Multi-activities',
+          link: '#'
+        },
+        {
+          textLink: 'Kayaking',
+          link: '#'
+        }
+      ],
+    },
+    {
+      title: 'Travel Blogs',
+      links: [
+        {
+          textLink: 'Bali Travel Guide',
+          link: '#'
+        },
+        {
+          textLink: 'Sri Links Travel Guide',
+          link: '#'
+        },
+        {
+          textLink: 'Peru Travel Guide',
+          link: '#'
+        },
+        {
+          textLink: 'Bali Travel Guide',
+          link: '#'
+        }
+      ],
+    },
+    {
+      title: 'about Us',
+      links: [
+        {
+          textLink: 'Our Story',
+          link: '#'
+        },
+        {
+          textLink: 'Work with us',
+          link: '#'
+        },
+
+      ],
+    },
+    {
+      title: 'Contact Us',
+      links: [
+        {
+          textLink: 'Our Story',
+          link: '#'
+        },
+        {
+          textLink: 'Work with us',
+          link: '#'
+        },
+
+      ],
+    },
+  ],
+  //_________________________________________PageItemNavigation
+  landingNavigations: [
+    {
+      title: 'Flights',
+      subtitle: 'Search Flights & Places Hire to our most popular destinations',
+      textBtn: 'Show Flights',
+      bg: 'flights',
+      route: '/flights'
+    },
+    {
+      title: 'Hotels',
+      subtitle: 'Search Flights & Places Hire to our most popular destinations',
+      textBtn: 'Show Hotels',
+      bg: 'hotels',
+      route: '/hotels'
+    }
+  ],
+  places: [
+    {
+      img: 'istanbul',
+      country: 'turkey',
+      city: 'istanbul',
+      links: {
+        flights: 'https://facebook.com',
+        hotels: 'https://twitter.com',
+        resorts: 'https://youtube.com',
+      }
+    },
+    {
+      img: 'sydney',
+      country: 'australia',
+      city: 'sydney',
+      links: {
+        flights: 'https://facebook.com',
+        hotels: 'https://twitter.com',
+        resorts: 'https://youtube.com',
+      }
+    },
+    {
+      img: 'baku',
+      country: 'azerbaijan',
+      city: 'baku',
+      links: {
+        flights: 'https://facebook.com',
+        hotels: 'https://twitter.com',
+        resorts: 'https://youtube.com',
+      }
+    },
+    {
+      img: 'male',
+      country: 'maldives',
+      city: 'malé',
+      links: {
+        flights: 'https://facebook.com',
+        hotels: 'https://twitter.com',
+        resorts: 'https://youtube.com',
+      }
+    },
+    {
+      img: 'paris',
+      country: 'france',
+      city: 'paris',
+      links: {
+        flights: 'https://facebook.com',
+        hotels: 'https://twitter.com',
+        resorts: 'https://youtube.com',
+      }
+    },
+    {
+      img: 'new-york',
+      country: 'US',
+      city: 'new york',
+      links: {
+        flights: 'https://facebook.com',
+        hotels: 'https://twitter.com',
+        resorts: 'https://youtube.com',
+      }
+    },
+    {
+      img: 'london',
+      country: 'UK',
+      city: 'london',
+      links: {
+        flights: 'https://facebook.com',
+        hotels: 'https://twitter.com',
+        resorts: 'https://youtube.com',
+      }
+    },
+    {
+      img: 'tokyo',
+      country: 'japan',
+      city: 'tokyo',
+      links: {
+        flights: 'https://facebook.com',
+        hotels: 'https://twitter.com',
+        resorts: 'https://youtube.com',
+      }
+    },
+    {
+      img: 'dubai',
+      country: 'UAE',
+      city: 'dubai',
+      links: {
+        flights: 'https://facebook.com',
+        hotels: 'https://twitter.com',
+        resorts: 'https://youtube.com',
+      }
+    }
+  ],
+  //______________________________________Reviews-component
+  reviewsItems: [
+    {
+      title: '“A real sense of community, nurtured”',
+      subtitle: 'Really appreciate the help and support from the staff during these tough times. Shutout to Katie for helping me always, even when I was out of the country. And always available when needed.',
+      link: 'https://facebook.com',
+      rating: '4',
+      userName: 'Olga',
+      userDescription: 'Weave Studios – Kai Tak',
+      icon: 'icon-google',
+      iconName: 'Google',
+      image: 'nature-1',
+    },
+    {
+      title: '“The facilities are superb. Clean, slick, bright.”',
+      subtitle: '“A real sense of community, nurtured”Really appreciate the help and support from the staff during these tough times. Shoutout to Katie for helping me always, even when I was out of the country. And always available when needed.View moreOlgaWeave Studios – Kai TakGoogle',
+      link: 'https://twitter.com',
+      rating: '5',
+      userName: 'Thomas',
+      icon: 'icon-google',
+      iconName: 'Google',
+      userDescription: 'Weave Studios – Olympic',
+      image: 'nature-2',
+    },
+    {
+      title: '“A real sense of community, nurtured”',
+      subtitle: 'Really appreciate the help and support from the staff during these tough times. Shutout to Katie for helping me always, even when I was out of the country. And always available when needed.',
+      link: 'https://youtube.com',
+      rating: '3',
+      userName: 'Eliot',
+      userDescription: 'Weave Studios – Kai Tak',
+      icon: 'icon-google',
+      iconName: 'Google',
+      image: 'nature-3',
+    },
+  ],
+}
+
+
+/*export default {
   socialMedia: [
     {
       icon: 'facebook',
@@ -21,4 +284,4 @@ export default {
   landingNavigations: [],
   places: [],
   reviewsItems: [],
-}
+}*/
