c647100d189a3cfe1c41066e4519b48e2a2dfd05
Hennadii Khlepitko <khlepitko.gennady@gmail.com>
2023-07-06 17:50:29 +0300

    vuelidate library added

--- src/main.js
+++ src/main.js
@@ -1,15 +1,19 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import { BootstrapVue } from 'bootstrap-vue';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
+import Vue       from "vue";
+import Vuelidate from "vuelidate";
+
+import App            from "./App.vue";
+import router         from "./router";
+import store          from "./store";
+import {BootstrapVue} from "bootstrap-vue";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
 Vue.use(BootstrapVue);
+Vue.use(Vuelidate);
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount("#app");
